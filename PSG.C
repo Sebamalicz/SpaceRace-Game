2f93df15ef6f7393a5f234c4b82fffab9dc0f098
Saint-Pablo <102761986+Saint-Pablo@users.noreply.github.com>
2022-04-02 13:18:13 -0600

    Update PSG.C

--- PSG.C
+++ PSG.C
@@ -3,9 +3,15 @@
 void write_psg(int reg, UINT8 val)
 {
 	long old_ssp;
-	old_ssp = Super(0);
-	
-	*psg_reg
+	if(reg >= 0 && reg <= 15)
+	{
+		old_ssp = Super(0);
+		
+		*psg_reg_select = reg;
+		*psg_reg_write = val;
+		
+		Super(old_ssp);
+	}
 }
 
 UINT8 read_psg(int reg)
@@ -16,26 +22,103 @@ UINT8 read_psg(int reg)
 
 void set_tone(int channel, int tuning)
 {
+	switch(channel)
+	{
+		case 0:
+			write_psg(CH_A_TONE, tuning);
+			break;
+		case 1:
+			write_psg(CH_B_TONE, tuning);
+			break;
+		case 2:
+			write_psg(CH_C_TONE, tuning);
+			break;
+	}
 	
 }
 
 void set_volume(int channel, int volume)
 {
+	switch(channel)
+	{
+		case 0:
+			write_psg(CH_A_VOL, tuning);
+			break;
+		case 1:
+			write_psg(CH_B_VOL, tuning);
+			break;
+		case 2:
+			write_psg(CH_C_VOL, tuning);
+			break;
+	}
 	
 }
 
 void enable_channel(int channel, int tone_on, int noise_on)
 {
+	bool tone_only = tone_on && !noise_on;
+  bool noise_only = !tone_on && noise_on;
+  bool tone_and_noise = tone_on && noise_on;
+  int existing_mixer_val = read_psg(MIXER_REG);
+  int channel_val;
+
+  switch(channel) {
+    case 0:
+      if (tone_only)
+        channel_val = existing_mixer_val != -1 ?
+                      existing_mixer_val & MIXER_TONE_CH_A : MIXER_TONE_CH_A;
+      else if (noise_only)
+        channel_val = existing_mixer_val != -1 ?
+                      existing_mixer_val & MIXER_NOISE_CH_A : MIXER_NOISE_CH_A;
+      else if (tone_and_noise)
+        channel_val = existing_mixer_val != -1 ?
+                      existing_mixer_val & (MIXER_TONE_CH_A & MIXER_NOISE_CH_A) :
+                      (MIXER_TONE_CH_A & MIXER_NOISE_CH_A);
+          break;
+    case 1:
+      if (tone_only)
+        channel_val = existing_mixer_val != -1 ?
+                      existing_mixer_val & MIXER_TONE_CH_B : MIXER_TONE_CH_B;
+      else if (noise_only)
+        channel_val = existing_mixer_val != -1 ?
+                      existing_mixer_val & MIXER_NOISE_CH_B : MIXER_NOISE_CH_B;
+      else if (tone_and_noise)
+        channel_val = existing_mixer_val != -1 ?
+                      existing_mixer_val & (MIXER_TONE_CH_B & MIXER_NOISE_CH_B) :
+                      (MIXER_TONE_CH_B & MIXER_NOISE_CH_B);
+          break;
+    case 2:
+      if (tone_only)
+        channel_val = existing_mixer_val != -1 ?
+                      existing_mixer_val & MIXER_TONE_CH_C : MIXER_TONE_CH_C;
+      else if (noise_only)
+        channel_val = existing_mixer_val != -1 ?
+                      existing_mixer_val & MIXER_NOISE_CH_C : MIXER_NOISE_CH_C;
+      else if (tone_and_noise)
+        channel_val = existing_mixer_val != -1 ?
+                      existing_mixer_val & (MIXER_TONE_CH_C & MIXER_NOISE_CH_C) :
+                      (MIXER_TONE_CH_C & MIXER_NOISE_CH_C);
+          break;
 	
 }
 
 void stop_sound()
 {
+	write_psg(MIXER_REG, 0x00);
+	write_psg(NOISE_FREQUENCY_REG, 0x00);
+	write_psg(ENVELOPE_FINE_REG, 0x00);
+	write_psg(ENVELOPE_ROUGH_REG, 0x00);
+	write_psg(ENVELOPE_SHAPE_CONTROL_REG, 0x00);
+
+	set_volume(0, 0);
+	set_volume(1, 0);
+	set_volume(2, 0);
 	
 }
 
 void set_noise(int tuning)
 {
+	wrtie_psg(NOISE_FREQUENCY_REG, tuning);
 	
 }
 
